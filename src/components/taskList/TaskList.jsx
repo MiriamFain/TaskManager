0784ae8fd1ba0600f17aa5f081c209e02bd177d2
Мириам Файнштейн <MIRIAM.FAINSHTEIN@gmail.com>
2023-04-28 15:41:07 +0300

    update Task List

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,30 +1,6 @@
 import TaskItem from '../taskItem/TaskItem';
 import './taskList.css';
 
-// const mockTasks = [
-//     {
-//         id: 1,
-//         description: 'Task 1',
-//         created_at: '2022-12-31 00:00:00',
-//         is_important: true,
-//         is_urgent: false,
-//     },
-//     {
-//         id: 2,
-//         description: 'Task 2',
-//         created_at: '2022-12-31 00:00:00',
-//         is_important: false,
-//         is_urgent: true,
-//     },
-//     {
-//         id: 3,
-//         description: 'Task 3',
-//         created_at: '2022-12-31 00:00:00',
-//         is_important: false,
-//         is_urgent: false,
-//     },
-// ];
-
 const TaskList = ({
     tasks,
     handleMarkImportant,
@@ -46,11 +22,10 @@ const TaskList = ({
                     handleMarkImportant={handleMarkImportant}
                     handleMarkUrgent={handleMarkUrgent}
                     onDelete={handleDelete}
-                    onEdit={handleEdit}
+                    handleEdit={handleEdit}
                     onComplete={handleMarkComplete}
                 />
             ))}
-            <button className="load_more">Load More</button>
         </div>
     );
 };
