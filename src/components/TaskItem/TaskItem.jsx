57963a21d00d82e508bba19485ea7b36a83754fd
Мириам Файнштейн <MIRIAM.FAINSHTEIN@gmail.com>
2023-04-28 15:41:53 +0300

    update Task Item

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../button/Button';
 import { AiOutlineCheck } from 'react-icons/ai';
 import { BsTrash } from 'react-icons/bs';
 import { AiOutlineEdit } from 'react-icons/ai';
+import moment from 'moment';
 
 import './taskItem.css';
 
@@ -11,12 +12,37 @@ const TaskItem = ({
     handleMarkImportant,
     handleMarkUrgent,
     onDelete,
-    onEdit,
     onComplete,
+    handleEdit,
 }) => {
     const { id, completed_at, description, created_at, important, urgent } =
         task;
     const isCompleted = Boolean(completed_at);
+
+    const momentObj = moment(created_at);
+    const formattedDate = momentObj.format('YYYY-MM-DD HH:mm:ss');
+
+    const [newDesc, setNewDesc] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const onChange = event => {
+        const value = event.target.value;
+        setNewDesc(value);
+    };
+
+    const onSubmit = () => {
+        handleEdit(id, newDesc);
+        setIsEditing(false);
+    };
+
+    useEffect(() => {
+        setNewDesc(description);
+    }, [description]);
+
+    const toggleEdit = () => {
+        isEditing && handleEdit(id, newDesc);
+        setIsEditing(prevState => !prevState);
+    };
 
     return (
         <div className="task-item">
@@ -28,27 +54,63 @@ const TaskItem = ({
                     <AiOutlineCheck />
                 </Button>
                 <div className="task-desc">
-                    <span className="desc">{description}</span>
-                    <span className="date">{created_at}</span>
+                    {isEditing ? (
+                        <form onSubmit={onSubmit}>
+                            <input
+                                className="task-edit__input"
+                                type="text"
+                                autoFocus
+                                value={newDesc}
+                                onChange={onChange}
+                            />
+                        </form>
+                    ) : (
+                        <>
+                            <span className="desc">{description}</span>
+                            <span className="date">{formattedDate}</span>
+                        </>
+                    )}
                 </div>
             </div>
             <div className="btn-list">
-                <button
-                    className={`btn-important ${important ? 'active' : ''}`}
-                    onClick={() => handleMarkImportant(id, !important)}
+                {isEditing ? (
+                    ''
+                ) : (
+                    <>
+                        <div>
+                            <button
+                                className={`btn-important ${
+                                    important ? 'active' : ''
+                                }`}
+                                onClick={() =>
+                                    handleMarkImportant(id, !important)
+                                }
+                            >
+                                Important
+                            </button>
+                            <button
+                                className={`btn-urgent ${
+                                    urgent ? 'active' : ''
+                                }`}
+                                onClick={() => handleMarkUrgent(id, !urgent)}
+                            >
+                                Urgent
+                            </button>
+                        </div>
+
+                        <Button
+                            className="btn--icon"
+                            onClick={() => onDelete(id)}
+                        >
+                            <BsTrash />
+                        </Button>
+                    </>
+                )}
+
+                <Button
+                    className={`btn--icon ${isEditing ? 'editing' : ''}`}
+                    onClick={toggleEdit}
                 >
-                    Important
-                </button>
-                <button
-                    className={`btn-urgent ${urgent ? 'active' : ''}`}
-                    onClick={() => handleMarkUrgent(id, !urgent)}
-                >
-                    Urgent
-                </button>{' '}
-                <Button className="btn--icon" onClick={() => onDelete(id)}>
-                    <BsTrash />
-                </Button>
-                <Button className="btn--icon" onClick={() => onEdit(id)}>
                     <AiOutlineEdit />
                 </Button>
             </div>
